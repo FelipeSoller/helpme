c9d55bbed6a209cab14df3b47999b19eae0b3ccc
Felipe Soller <soller@outlook.com>
2020-07-06 15:37:28 -0300

    Reuso de códigos com nunjucks e configurando retorno de datas

--- teachers.js
+++ teachers.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const data = require("./data.json")
-const { age } = require("./utils")
+const { age, date } = require("./utils")
 const Intl =require('intl')
 
 exports.show = function(req, res) {
@@ -53,4 +53,19 @@ exports.post = function(req, res) {
 
         return res.redirect("teachers")
     })    
+}
+
+exports.edit = function(req, res) {
+    const { id } = req.params 
+
+    const foundTeacher = data.teachers.find(function(teacher){
+        return teacher.id == id
+    })
+    if (!foundTeacher) return res.send("Teacher not found")
+
+    const teacher = {
+        ...foundTeacher,
+        birth: date(foundTeacher.birth)
+    }   
+    return res.render('teachers/edit', { teacher })
 }
